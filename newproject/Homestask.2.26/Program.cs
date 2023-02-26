2322f60cffbdceaf3a86f37e17f79d1d05e67e60
Nigar-Mahammadova <114868440+Nigar-Mahammadova@users.noreply.github.com>
2023-02-26 23:32:54 +0400

    Add files via upload

---
+++ newproject/Homestask.2.26/Program.cs
@@ -0,0 +1,58 @@
+﻿//Verilmish ededin menfi ve ya musbet oldugunu tapan algoritmasi
+
+//int n;
+//Console.WriteLine("Input Any Number");
+//n = int.Parse(Console.ReadLine());
+
+//if (n >= 0)
+//    if (n == 0)
+//    {
+//        Console.WriteLine("Entered Number is Zero");
+
+//    }
+//else
+//{
+//    Console.WriteLine(" {0} is a Positive Number ", n);
+//}
+//else
+//{
+//    Console.WriteLine(" {0} is a Negative Number ", n);
+//}
+
+
+//Verilmish ededin tek ve ya cut oldugunu tapan algoritma
+
+//int i;
+//Console.Write("Enter the Number");
+//i = int.Parse(Console.ReadLine());
+
+//if(i%2==0)
+//{
+//    Console.WriteLine(i + "Number is Odd");
+//}
+//else
+//{
+//    Console.WriteLine(i + "Number is Even");
+//}
+
+//Dogum tarıxı verılır uzerıne 18 gelırsız eger netıce 2023 den boyukdurse ekrana 18 yashınız tamam olmayıb,kicikdirse tamam olub yazisini cixaran algoritma yazirsiniz.
+
+//{
+//    {
+//        int vote_age;
+
+
+//        Console.Write("Yasinizi daxil edin : ");
+//        vote_age = Convert.ToInt32(Console.ReadLine());
+//        if (vote_age < 18)
+//        {
+//            Console.Write("Ne edek, 18 yasiniz tamam olmayib, tamam olandan sonra gelersiniz\n");
+//            Console.Write("Ancaq ki, {0} ilden sonra kod yaza bilersiniz\n\n", 18 - vote_age);
+//        }
+//        else
+//            Console.Write("Tebrikler, Mukemmel! 18 yasiniz tamam olub\n\n");
+//    }
+//}
+
+
+
---
+++ newproject/main.js
@@ -0,0 +1,9 @@
+let number = 8
+let remainder = (number % 2)
+
+if (remainder == 0) {
+    console.log('${number} is even')
+} else {
+    console.log('${number} is odd')
+}
+
